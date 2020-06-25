1e5f935bd29b5018b3243f44395d9a2ac8f52753
jwelly <64269613+jwelly@users.noreply.github.com>
2020-06-25 15:46:06 +0100

    Updated color-event-types JS notes

--- color-event-types/main.js
+++ color-event-types/main.js
@@ -9,11 +9,19 @@ function rgb(num) {
   return Math.floor(Math.random() * num);
 }
 
-// Write your code below
+// Write your code below, with 256, we include the option of pure white
+// The 3 rgb(256) in the middle are CALLING the rgb function above, with 256 as it's argument
+// The rgb function returns a random number, 3 times, to this colorChange function
+// The result is, for example: randomColor = rgb(224, 56, 139)
 let colorChange = function (event) {
-  let randomColor = 'rgb(' + rgb(255) + ',' + rgb(255) + ',' + rgb(255) + ')';
+  let randomColor = 'rgb(' + rgb(256) + ',' + rgb(256) + ',' + rgb(256) + ')';
   event.target.style.backgroundColor = randomColor;
 }
+/* In template literal form it would be:
+    `rgb(${rgb(256)}, ${rgb(256)}, ${rgb(256)})`
+  HOWEVER, it has to be in string form to assign it in script
+*/
+
 
 button.onclick = colorChange;
 mysteryButton.onwheel = colorChange;
